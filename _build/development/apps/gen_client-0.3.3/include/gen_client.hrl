840af4ebb96d6e8d63a679bd746d23f972880732
Boris Okner <b.okner@rogers.com>
2010-07-12 21:37:10 -0400

    cleanup

--- _build/development/apps/gen_client-0.3.3/include/gen_client.hrl
+++
@@ -1,5 +0,0 @@
-% State
--record(client_state, {session, jid, handlers, terminators, options = [], args = []}).
-% Adhoc command
--record(command, {id, name, handler}).
--record(command_result, {id, result, status, sessionid}).
